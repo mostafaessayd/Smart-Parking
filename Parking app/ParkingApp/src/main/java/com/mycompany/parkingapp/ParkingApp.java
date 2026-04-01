ae483137865b2e19904496f52c141043d4d00b77
mohamedessayd <ballm3957@gmail.com>
2026-04-01 14:21:00 +0100

    new

--- Parking app/ParkingApp/src/main/java/com/mycompany/parkingapp/ParkingApp.java
+++ Parking app/ParkingApp/src/main/java/com/mycompany/parkingapp/ParkingApp.java
@@ -5,35 +5,35 @@ import java.util.Scanner;
 public class ParkingApp {
 
     public static void main(String[] args) {
-        // Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
-        // System.out.println("💻 Projet Arduino Uploader");
+        System.out.println("💻 Projet Arduino Uploader");
 
-        // // Saisie du chemin du projet
-        // String projectPath = "C:\\Users\\DZ Laptops\\Documents\\Arduino\\projet".trim();
+        // Saisie du chemin du projet
+        String projectPath = "C:\\Users\\DZ Laptops\\Documents\\Arduino\\projet".trim();
 
-        // // Saisie du port
-        // String port = "COM5".trim();
+        // Saisie du port
+        String port = "COM5".trim();
 
-        // // Création de l'objet
-        // ArduinoUploader uploader = new ArduinoUploader(projectPath, port);
+        // Création de l'objet
+        ArduinoUploader uploader = new ArduinoUploader(projectPath, port);
 
-        // try {
-        //     // Upload du code
-        //     uploader.uploadProject();
+        try {
+            // Upload du code
+            uploader.uploadProject();
 
-        //     System.out.println("✅ Le code a été téléchargé sur la carte.");
+            System.out.println("✅ Le code a été téléchargé sur la carte.");
 
-        //     // Contrôle après upload
-        //     System.out.println("Vous pouvez maintenant contrôler la carte Arduino via Serial en Java.");
+            // Contrôle après upload
+            System.out.println("Vous pouvez maintenant contrôler la carte Arduino via Serial en Java.");
 
-        // } catch (Exception e) {
-        //     System.out.println("⚠️ Une erreur s'est produite أثناء la compilation ou le téléchargement :");
-        //     e.printStackTrace();
-        // }
+        } catch (Exception e) {
+            System.out.println("⚠️ Une erreur s'est produite أثناء la compilation ou le téléchargement :");
+            e.printStackTrace();
+        }
 
-        // input.close();
-        // System.out.println("Fin du programme.");
+        input.close();
+        System.out.println("Fin du programme.");
 
         try {
             ArduinoLogger logger = new ArduinoLogger("COM5");
